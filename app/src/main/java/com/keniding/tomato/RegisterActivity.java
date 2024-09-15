38936e95c3533bbbd0c68a350d0d4191979f50d0
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2024-09-15 00:22:58 -0500

    registro
    registro

--- app/src/main/java/com/keniding/tomato/RegisterActivity.java
+++ app/src/main/java/com/keniding/tomato/RegisterActivity.java
@@ -20,6 +20,9 @@ public class RegisterActivity extends AppCompatActivity {
         ActivityRegisterBinding binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.setTitleText("Tomato");
+        binding.setSubtitleText("Welcome");
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
